add44f135462573dd6420802bcd4f49d2e2f5cbf
damencho <damencho@jitsi.org>
2021-06-29 13:02:21 +0300

    feat: Introduces new states for e2ee feature.
    Stores everyoneSupportsE2EE and everyoneEnabledE2EE to minimize looping through participants list.

--- react/features/e2ee/middleware.js
+++ react/features/e2ee/middleware.js
@@ -2,11 +2,22 @@
 
 import { APP_WILL_MOUNT, APP_WILL_UNMOUNT } from '../base/app';
 import { getCurrentConference } from '../base/conference';
-import { getLocalParticipant, participantUpdated } from '../base/participants';
+import {
+    getLocalParticipant,
+    getParticipants,
+    PARTICIPANT_JOINED,
+    PARTICIPANT_LEFT,
+    PARTICIPANT_UPDATED,
+    participantUpdated
+} from '../base/participants';
 import { MiddlewareRegistry, StateListenerRegistry } from '../base/redux';
 import { playSound, registerSound, unregisterSound } from '../base/sounds';
 
-import { TOGGLE_E2EE } from './actionTypes';
+import {
+    SET_EVERYONE_ENABLED,
+    SET_EVERYONE_SUPPORTS,
+    TOGGLE_E2EE
+} from './actionTypes';
 import { toggleE2EE } from './actions';
 import { E2EE_OFF_SOUND_ID, E2EE_ON_SOUND_ID } from './constants';
 import logger from './logger';
@@ -34,6 +45,69 @@ MiddlewareRegistry.register(({ dispatch, getState }) => next => action => {
         dispatch(unregisterSound(E2EE_OFF_SOUND_ID));
         dispatch(unregisterSound(E2EE_ON_SOUND_ID));
         break;
+
+    case PARTICIPANT_UPDATED:
+    case PARTICIPANT_JOINED: {
+        const result = next(action);
+        const { e2eeEnabled, e2eeSupported, local } = action.participant;
+        const { everyoneEnabledE2EE } = getState()['features/e2ee'];
+
+        // if all had it enabled and this one disabled it, change value in store
+        // otherwise there is no change in the value we store
+        if (everyoneEnabledE2EE && !e2eeEnabled) {
+            dispatch({
+                type: SET_EVERYONE_ENABLED,
+                everyoneEnabledE2EE: false
+            });
+        }
+
+        if (local) {
+            return result;
+        }
+
+        const { everyoneSupportsE2EE } = getState()['features/e2ee'];
+
+        // if all supported it and this one does not, change value in store
+        // otherwise there is no change in the value we store
+        if (everyoneSupportsE2EE && !e2eeSupported) {
+            dispatch({
+                type: SET_EVERYONE_SUPPORTS,
+                everyoneSupportsE2EE: false
+            });
+        }
+
+        return result;
+    }
+
+    case PARTICIPANT_LEFT: {
+        const result = next(action);
+        const { e2eeEnabled, e2eeSupported } = action.participant;
+
+        const { everyoneEnabledE2EE } = getState()['features/e2ee'];
+
+        const participants = getParticipants(getState());
+
+        // if it was not enabled by everyone, and the participant leaving had it disabled
+        // let's check is it enabled for all that stay
+        if (!everyoneEnabledE2EE && !e2eeEnabled && participants.every(p => p.e2eeEnabled)) {
+            dispatch({
+                type: SET_EVERYONE_ENABLED,
+                everyoneEnabledE2EE: true
+            });
+        }
+        const { everyoneSupportsE2EE } = getState()['features/e2ee'];
+
+        // if it was not supported by everyone, and the participant leaving had it not supported
+        // let's check is it supported by all that stay behind
+        if (!everyoneSupportsE2EE && !e2eeSupported && participants.every(p => p.local || p.e2eeSupported)) {
+            dispatch({
+                type: SET_EVERYONE_SUPPORTS,
+                everyoneSupportsE2EE: true
+            });
+        }
+
+        return result;
+    }
 
     case TOGGLE_E2EE: {
         const conference = getCurrentConference(getState);
